b69f6ceb2ab6a9eec4ab0af55baddfa8d0157f50
BayramWilson <bayram.wilson04@gmail.com>
2025-12-03 14:26:13 +0100

    updated tolower and toupper

--- libft/ft_toupper.c
+++ libft/ft_toupper.c
@@ -6,13 +6,14 @@
 /*   By: bwilson <bwilson@student.42Berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/02 12:29:00 by bwilson           #+#    #+#             */
-/*   Updated: 2025/12/03 13:27:55 by bwilson          ###   ########.fr       */
+/*   Updated: 2025/12/03 14:21:23 by bwilson          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 void	ft_toupper(char x)
 {
-	x = x - 32;
+	if (x >= 'a' && x <= 'z')
+		x = x - 32;
 }
 /*
 #include <ctype.h>
