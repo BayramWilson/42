c4c93166bd5dc9d0c977c7a0d2941985070bbb22
BayramWilson <bayram.wilson04@gmail.com>
2025-12-04 14:17:46 +0100

    completed and tested atoi

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -6,42 +6,60 @@
 /*   By: bwilson <bwilson@student.42Berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/03 19:58:17 by bwilson           #+#    #+#             */
-/*   Updated: 2025/12/04 12:18:04 by bwilson          ###   ########.fr       */
+/*   Updated: 2025/12/04 14:15:56 by bwilson          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int ft_isspace(char x)
+int	ft_isdigit(char x)
 {
-    if((x >= '\t' && x <= '\r') || x == ' ')
-        return 1;
-    return 0;
+	if (x >= '0' && x <= '9')
+		return (1);
+	else
+		return (0);
 }
 
-int ft_atoi(char *str)
+int	ft_isspace(char x)
 {
-    int i;
-    i = 0;
-
-    int result = 0;
-    while(str[i])
-    {
-        while (ft_isspace(str[i]))
-            i++;
-        
-        if(str[i] >= '0' && str[i] <= '9')
-        {
-            result = str[i] - 48;
-            break;
-        }
-    }
-    return result;
+	if ((x >= '\t' && x <= '\r') || x == ' ')
+		return (1);
+	return (0);
 }
 
+int	ft_atoi(char *str)
+{
+	int	i;
+	int	result;
+	int	symbol;
+
+	i = 0;
+	result = 0;
+	symbol = 1;
+	while (str[i])
+	{
+		while (ft_isspace(str[i]))
+			i++;
+		if (str[i] == '-' || str[i] == '+')
+		{
+			if (str[i] == '-')
+				symbol = -symbol;
+			i++;
+		}
+		while (ft_isdigit(str[i]))
+		{
+			result = result * 10 + (str[i] - '0');
+			i++;
+		}
+		return (result * symbol);
+	}
+	return (0);
+}
+/*
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void) {
-  char str[] = "3";
+int	main(void) {
+  char str[] = "-323a";
   int amount = ft_atoi(str);
   printf("%d\n", amount);
 }
+*/
