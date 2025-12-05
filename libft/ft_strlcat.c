d10a9b036c472d28e63630e6d2b81ad31edf8fac
Rammo Dev <bayram.wilson04@gmail.com>
2025-12-05 10:08:26 +0100

    commented out not working files

--- libft/ft_strlcat.c
+++ libft/ft_strlcat.c
@@ -3,13 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strlcat.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bwilson <bwilson@student.42Berlin.de>      +#+  +:+       +#+        */
+/*   By: bwilson <bwilson@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/03 14:35:12 by bwilson           #+#    #+#             */
-/*   Updated: 2025/12/04 15:23:15 by bwilson          ###   ########.fr       */
+/*   Updated: 2025/12/05 10:02:04 by bwilson          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+/*
 char ft_strlcat(char *dest, char *src, int n)
 {
     
@@ -27,3 +28,4 @@ int main(void)
     printf("%s\n%d", dest, result);
 
 }
+*/
