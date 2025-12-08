46c687cc05effcf9ae36b3c4633d571c8bb2341b
BayramWilson <bayram.wilson04@gmail.com>
2025-12-08 14:52:26 +0100

    norminette memchr

--- libft/ft_memchr.c
+++ libft/ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: bwilson <bwilson@student.42Berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/08 14:22:06 by bwilson           #+#    #+#             */
-/*   Updated: 2025/12/08 14:44:42 by bwilson          ###   ########.fr       */
+/*   Updated: 2025/12/08 14:51:13 by bwilson          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,14 @@
 void	*ft_memchr(const void *s, int c, size_t n)
 {
 	unsigned char	*ps;
-    
-    ps = (unsigned char *)s;
-    while(n > 0)
-    {
-        if (*ps == (unsigned char)c)
-            return ((void *)ps);
-        ps++;
-        n--;
-    }
-    return (NULL);
+
+	ps = (unsigned char *)s;
+	while (n > 0)
+	{
+		if (*ps == (unsigned char)c)
+			return ((void *)ps);
+		ps++;
+		n--;
+	}
+	return (NULL);
 }
