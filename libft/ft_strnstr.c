83f2af4b85b98b876cfa99c8e2731d1e417ec8df
BayramWilson <bayram.wilson04@gmail.com>
2025-12-08 15:11:46 +0100

    done ft_strnstr.c

--- libft/ft_strnstr.c
+++ libft/ft_strnstr.c
@@ -6,7 +6,30 @@
 /*   By: bwilson <bwilson@student.42Berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/08 12:57:41 by bwilson           #+#    #+#             */
-/*   Updated: 2025/12/08 13:13:57 by bwilson          ###   ########.fr       */
+/*   Updated: 2025/12/08 15:11:13 by bwilson          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stddef.h>
+
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
+{
+	size_t	i;
+	size_t	j;
+
+	if (needle[0] == '\0')
+		return ((char *)haystack);
+	i = 0;
+	while (haystack[i] && i < len)
+	{
+		j = 0;
+		while (haystack[i + j] == needle[j] && (i + j) < len)
+		{
+			if (needle[j + 1] == '\0')
+				return ((char *)&haystack[i]);
+			j++;
+		}
+		i++;
+	}
+	return (NULL);
+}
