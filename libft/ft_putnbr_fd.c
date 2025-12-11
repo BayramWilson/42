1d6b38d396dec08e12d52878c144b280bbb7fec6
Rammo Dev <bayram.wilson04@gmail.com>
2025-12-11 22:34:27 +0100

    remove unnecessary include

--- libft/ft_putnbr_fd.c
+++ libft/ft_putnbr_fd.c
@@ -6,18 +6,17 @@
 /*   By: bwilson <bwilson@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/11 19:18:15 by bwilson           #+#    #+#             */
-/*   Updated: 2025/12/11 22:26:29 by bwilson          ###   ########.fr       */
+/*   Updated: 2025/12/11 22:32:31 by bwilson          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <unistd.h>
-#include <stddef.h>
 
 void	ft_putnbr_fd(int n, int fd)
 {
+	int	    i;
 	char	*s;
-	size_t	i;
 
 	i = 0;
 	s = ft_itoa(n);
