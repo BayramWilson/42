02b92d72711f523abd58835c3d8b6e5b7a3fc400
Bayram Wilson <bwilson@c4b4c3.42berlin.de>
2025-12-15 19:48:34 +0100

    done

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -6,66 +6,69 @@
 /*   By: bwilson <bwilson@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/15 17:49:19 by bwilson           #+#    #+#             */
-/*   Updated: 2025/12/15 19:14:43 by bwilson          ###   ########.fr       */
+/*   Updated: 2025/12/15 19:34:00 by bwilson          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stddef.h>
 #include "libft.h"
+#include <stddef.h>
 
-int count_words(char const *s, char c)
+int	count_words(char const *s, char c)
 {
-    size_t  i;
-    size_t  words;
-    int     is_word;
+	size_t	i;
+	size_t	words;
+	int		is_word;
 
-    is_word = 0;
-    i = 0;
-    words = 0;
-    while(s[i])
-    {
-        if(s[i] != c && is_word == 0)
-        {
-            is_word = 1;
-            words++;
-        }
-        else if(s[i] == c)
-            is_word = 0;
-        i++;
-    }
-    return words;
+	is_word = 0;
+	i = 0;
+	words = 0;
+	while (s[i])
+	{
+		if (s[i] != c && is_word == 0)
+		{
+			is_word = 1;
+			words++;
+		}
+		else if (s[i] == c)
+			is_word = 0;
+		i++;
+	}
+	return (words);
 }
 
-char **extract_word(char const *s, char c)
+int	size_word(char const *s, char c, int i)
 {
-    size_t  i;
-    int     j;
-    int     is_word;
-    int     start;
-    char    *temp;
+	int	size;
 
-    i = 0;
-    is_word = 0;
-    j = 0;
-    start = 0;
-    while(s[i])
-    {
-        if(s[i] != c && is_word == 0)
-        {
-            is_word = 1;
-            j++;
-        }
-        else if(s[i] == c)
-            is_word = 0;
-        i++;
-    }
+	size = 0;
+	while (s[i] != c && s[i])
+	{
+		size++;
+		i++;
+	}
+	return (size);
 }
 
-char **ft_split(char const *s, char c)
+char	**ft_split(char const *s, char c)
 {
-    char **result;
+	int		i;
+	int		j;
+	int		size;
+	char	**result;
 
-    result = malloc(sizeof(char *) * (count_words(s, c) + 1));
-
-    return result;
+	i = 0;
+	j = -1;
+	result = (char **)malloc(sizeof(char *) * (count_words(s, c) + 1));
+	if (!result)
+		return (0);
+	while (++j < count_words(s, c))
+	{
+		while (s[i] == c)
+			i++;
+		size = size_word(s, c, i);
+		result[j] = ft_substr(s, i, size);
+		i = i + size;
+	}
+	result[j] = 0;
+	return (result);
 }
