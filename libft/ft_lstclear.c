04553ebacef69aaa8e0a29f5b9f0635645810c98
BayramWilson <bayram.wilson04@gmail.com>
2025-12-15 14:17:27 +0100

    feat: add ft_lstclear function to delete all nodes in a list; update libft.h

---
+++ libft/ft_lstclear.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstclear.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bwilson <bwilson@student.42berlin.de>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/15 14:13:48 by bwilson           #+#    #+#             */
+/*   Updated: 2025/12/15 14:16:33 by bwilson          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstclear(t_list **lst, void (*del)(void *))
+{
+	t_list	*tmp;
+
+	while (*lst != NULL)
+	{
+		tmp = (*lst)->next;
+		ft_lstdelone(*lst, del);
+		*lst = tmp;
+	}
+}
