3af9d4cf9f52386c17a7b4e2b01bd11043a071a5
IlluminatisSon <121392126+BayramWilson@users.noreply.github.com>
2026-01-08 17:39:35 +0100

    Remove ft_strjoin function from utils
    Removed the ft_strjoin function implementation from get_next_line_utils.c.

--- rank01/get_next_line/get_next_line_utils.c
+++ rank01/get_next_line/get_next_line_utils.c
@@ -9,33 +9,3 @@
 /*   Updated: 2026/01/06 14:38:39 by bwilson          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include <stddef.h>
-
-char	*ft_strjoin(char const *s1, char const *s2)
-{
-	size_t	i;
-	size_t	all_size;
-	char	*result;
-
-	if (!s1 || !s2)
-	{
-		if (s1)
-			return (ft_strdup(s1));
-		else if (s2)
-			return (ft_strdup(s2));
-		else
-			return (ft_strdup(""));
-	}
-	all_size = ft_strlen(s1) + ft_strlen(s2);
-	result = (char *)malloc(all_size + 1);
-	if (!result)
-		return (result);
-	i = 0;
-	while (*s1)
-		result[i++] = *s1++;
-	while (*s2)
-		result[i++] = *s2++;
-	result[i] = '\0';
-	return (result);
-}
