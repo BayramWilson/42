3aff1970a957234a734571ce8819aaca78df7022
BayramWilson <bayram.wilson04@gmail.com>
2026-01-30 11:09:31 +0100

    total added, total growth, types, height validation implemented

--- rank02/python_modules/module01/ex6/ft_garden_analytics.py
+++ rank02/python_modules/module01/ex6/ft_garden_analytics.py
@@ -20,23 +20,34 @@ class GardenManager:
                      amount_plants: int,
                      growth_plants: int,
                      gardens_managed: int,
+                     regular: int,
+                     flowering: int,
+                     prize_flowers: int
                      ) -> None:
             self.score = score
             self.plant = plant
             self.amount_plants = amount_plants
             self.growth_plants = growth_plants
             self.gardens_managed = gardens_managed
+            self.regular = regular
+            self.flowering = flowering
+            self.prize_flowers = prize_flowers
 
         @staticmethod
-        def owner_report(owner: str, plant: object, first_call: bool) -> None:
+        def owner_report(owner: str, plant: object, first_call: bool) -> list:
+            regular = 0
+            flowering = 0
+            prize_flowers = 0
             if first_call:
-                print(f"=== {owner}'s Garden Report")
+                print(f"\n=== {owner}'s Garden Report\n")
                 print("Plants in garden:")
 
             # color, prize flower, blooming
             if (plant.color != ""
                 and isinstance(plant, PrizeFlower)
                     and plant.blooming):
+                flowering += 1
+                prize_flowers += 1
                 print(f"- {plant.name}: "
                       f"{plant.height}cm, {plant.color} (blooming), "
                       f"Prize points: {plant.prize_points}")
@@ -45,6 +56,8 @@ class GardenManager:
             elif (plant.color == ""
                     and isinstance(plant, PrizeFlower)
                     and plant.blooming):
+                flowering += 1
+                prize_flowers += 1
                 print(f"- {plant.name}: "
                       f"{plant.height}cm (blooming), "
                       f"Prize points: {plant.prize_points}")
@@ -53,6 +66,7 @@ class GardenManager:
             elif (plant.color != ""
                     and not isinstance(plant, PrizeFlower)
                     and plant.blooming):
+                flowering += 1
                 print(f"- {plant.name}: "
                       f"{plant.height}cm, {plant.color} (blooming)")
 
@@ -60,6 +74,7 @@ class GardenManager:
             elif (plant.color == ""
                     and not isinstance(plant, PrizeFlower)
                     and plant.blooming):
+                flowering += 1
                 print(f"- {plant.name}: "
                       f"{plant.height}cm (blooming)")
 
@@ -67,6 +82,7 @@ class GardenManager:
             elif (plant.color != ""
                     and isinstance(plant, PrizeFlower)
                     and not plant.blooming):
+                prize_flowers += 1
                 print(f"- {plant.name}: "
                       f"{plant.height}cm, {plant.color}, "
                       f"Prize points: {plant.prize_points}")
@@ -75,6 +91,7 @@ class GardenManager:
             elif (plant.color == ""
                     and isinstance(plant, PrizeFlower)
                     and not plant.blooming):
+                prize_flowers += 1
                 print(f"- {plant.name}: "
                       f"{plant.height}cm, "
                       f"Prize points: {plant.prize_points}")
@@ -83,6 +100,7 @@ class GardenManager:
             elif (plant.color != ""
                     and not isinstance(plant, PrizeFlower)
                     and not plant.blooming):
+                regular += 1
                 print(f"- {plant.name}: "
                       f"{plant.height}cm, {plant.color}")
 
@@ -90,16 +108,48 @@ class GardenManager:
             elif (plant.color == ""
                     and not isinstance(plant, PrizeFlower)
                     and not plant.blooming):
+                regular += 1
                 print(f"- {plant.name}: "
                       f"{plant.height}cm")
-
+            return [regular, flowering, prize_flowers]
+            
         @staticmethod
-        def print_owner_report(list_of_plants: list, owner: str) -> None:
+        def print_owner_report(list_of_plants: list,
+                               owner: str,
+                               total_growth: int
+                               ) -> None:
             first_call = True
+            regular_sum = 0
+            flowering_sum = 0
+            prize_flower_sum = 0
+            plants_added = 0
             fn = GardenManager.GardenStats
             for plant in list_of_plants:
-                fn.owner_report(owner, plant, first_call)
+                r, f, p = fn.owner_report(owner, plant, first_call)
+                regular_sum += r
+                flowering_sum += f
+                prize_flower_sum += p
                 first_call = False
+                plants_added += 1
+            print(f"\nPlants added: {plants_added}, "
+                  f"Total growth: {total_growth}")
+            print(f"Plant types: {regular_sum} regular, "
+                  f"{flowering_sum} flowering, "
+                  f"{prize_flower_sum} prize flowers")
+            if (GardenManager.GardenStats.height_validation(list_of_plants)):
+                print("Height validaton test: True")
+            else:
+                print("Height validaton test: False")
+
+        def height_validation(list_of_plants: list) -> bool:
+            false_counter = 0
+            for plant in list_of_plants:
+                if (plant.height < 0):
+                    false_counter += 1
+            if false_counter > 0:
+                return False
+            else:
+                return True
 
 
 class FloweringPlant(Plant):
@@ -118,17 +168,19 @@ class FloweringPlant(Plant):
         print(f"Added {name} to {garden.garden_name}'s garden")
 
     @staticmethod
-    def print_flowering(list_of_plants: list) -> None:
+    def print_flowering(list_of_plants: list, total_growth) -> int:
         first_call = True
         for plant in list_of_plants:
             FloweringPlant.flowering(plant, first_call)
             first_call = False
+            total_growth += 1
+        return total_growth
 
     @staticmethod
     def flowering(plant: object, first_call: bool) -> None:
         garden_name = plant.garden.garden_name
         if first_call:
-            print(f"{garden_name} is helping all Plants grow...")
+            print(f"\n{garden_name} is helping all Plants grow...")
         print(f"- {plant.name} grew 1cm")
         plant.height += 1
 
@@ -147,7 +199,7 @@ class PrizeFlower(FloweringPlant):
 
 
 if __name__ == "__main__":
-    print("=== Garden Management System Demo ===")
+    print("=== Garden Management System Demo ===\n")
     # first_call = True
     g0 = GardenManager("Alice")
     p0 = FloweringPlant(name="Rose",
@@ -157,7 +209,7 @@ if __name__ == "__main__":
                         blooming=True,
                         garden=g0)
     p1 = FloweringPlant(name="Oak Tree",
-                        height=187,
+                        height=-187,
                         age=1337,
                         color="",
                         blooming=False,
@@ -170,10 +222,11 @@ if __name__ == "__main__":
                      garden=g0,
                      prize_points=10)
     list_of_plants = [p0, p1, p2]
-    FloweringPlant.print_flowering(list_of_plants)
-
+    total_growth = 0
+    total_growth = FloweringPlant.print_flowering(list_of_plants, total_growth)
+    total_growth = FloweringPlant.print_flowering(list_of_plants, total_growth)
     GardenManager.GardenStats.print_owner_report(list_of_plants,
-                                                 g0.garden_name)
+                                                 g0.garden_name, total_growth)
     # g1 = GardenManager("Alice")
     # g1_stats = g1.GardenStats(22)
     # p1 = GardenManager.FloweringPlant("Rose", 30, 12)
