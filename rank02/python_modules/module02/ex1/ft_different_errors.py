2e97c9037c877070b553b196d74fe8b8699539d6
Rammo Dev <bayram.wilson04@gmail.com>
2026-01-31 00:21:03 +0100

    ex1 almost done and started ex2

--- rank02/python_modules/module02/ex1/ft_different_errors.py
+++ rank02/python_modules/module02/ex1/ft_different_errors.py
@@ -1,8 +1,57 @@
+from typing import TextIO
+
 def garden_operations() -> None:
+    a_str = "abc"
+    value(a_str)
+    a_zero = 0
+    zero_division(a_zero)
+    a_file = "tess.txt"
+    file_not_found(a_file)
+    a_dict = {"a": 1, "b": 2}
+    key(a_dict, "c")
+    
+    
+
+def test_error_types() -> None:
     pass
-def helper(test: str) -> None:
+
+# ERROR FUNCTIONS
+
+# ValueError
+def value(test: str) -> None:
+    print("Testing ValueError...")
     try:
-        print("Testing ValueError...")
         test = int(test)
     except ValueError:
-        
+        print("Caught ValueError: invalid literal for int()\n")
+
+# ZeroDivisionError
+def zero_division(test: int) -> None:
+    print("Testing ZeroDivisionError...")
+    try:
+        test = 10 / test
+    except ZeroDivisionError:
+        print("Caught ZeroDivisionError: division by zero\n")
+
+# FileNotFoundError
+def file_not_found(test: TextIO) -> None:
+    print("Testing FileNotFoundError...")
+    try:
+        f = open(test, "r")
+        f.close()
+    except FileNotFoundError:
+        print(f"Caught FileNotFoundError: No such file {test}\n")
+
+# KeyError
+def key(test: dict[str, int], key: str) -> None:
+    print("Testing KeyError...")
+    try:
+        print(test[key])
+    except KeyError:
+        print(f"Caught KeyError: {key}\n")
+    
+
+if __name__ == "__main__":
+    print("=== Garden Error Types Demo ===")
+    garden_operations()
+    pass
