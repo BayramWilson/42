fa42dbfd503831632841d96eebc2eacb195f8a09
BayramWilson <bayram.wilson04@gmail.com>
2026-02-01 19:55:44 +0100

    Refactor error handling in garden_operations and add test_error_types function

--- rank02/python_modules/module02/ex1/ft_different_errors.py
+++ rank02/python_modules/module02/ex1/ft_different_errors.py
@@ -1,22 +1,46 @@
-from typing import TextIO
+from typing import Any
+
 
 def garden_operations() -> None:
-    a_str = "abc"
+    a_str: str = "abc"
     value(a_str)
-    a_zero = 0
+    a_zero: int = 0
     zero_division(a_zero)
-    a_file = "tess.txt"
+    a_file: Any = "tess.txt"
     file_not_found(a_file)
-    a_dict = {"a": 1, "b": 2}
+    a_dict: dict = {"a": 1, "b": 2}
     key(a_dict, "c")
-    
-    
+
 
 def test_error_types() -> None:
-    pass
+    print("Testing multiple errors together...")
+    a_str: str = "abc"
+    a_zero: int = 0
+    a_file: Any = "tess.txt"
+    a_dict: dict = {"a": 1, "b": 2}
+
+    try:
+        int(a_str)
+    except ValueError:
+        print("Caught an error, but program continues!")
+    try:
+        a_zero = 10 / a_zero
+    except ZeroDivisionError:
+        print("Caught an error, but program continues!")
+
+    try:
+        f = open(a_file, "r")
+        f.close()
+    except FileNotFoundError:
+        print("Caught an error, but program continues!")
+
+    try:
+        print(a_dict[key])
+    except KeyError:
+        print("Caught an error, but program continues!")
+
 
 # ERROR FUNCTIONS
-
 # ValueError
 def value(test: str) -> None:
     print("Testing ValueError...")
@@ -24,6 +48,7 @@ def value(test: str) -> None:
         test = int(test)
     except ValueError:
         print("Caught ValueError: invalid literal for int()\n")
+
 
 # ZeroDivisionError
 def zero_division(test: int) -> None:
@@ -33,14 +58,16 @@ def zero_division(test: int) -> None:
     except ZeroDivisionError:
         print("Caught ZeroDivisionError: division by zero\n")
 
+
 # FileNotFoundError
-def file_not_found(test: TextIO) -> None:
+def file_not_found(test: Any) -> None:
     print("Testing FileNotFoundError...")
     try:
         f = open(test, "r")
         f.close()
     except FileNotFoundError:
         print(f"Caught FileNotFoundError: No such file {test}\n")
+
 
 # KeyError
 def key(test: dict[str, int], key: str) -> None:
@@ -49,9 +76,10 @@ def key(test: dict[str, int], key: str) -> None:
         print(test[key])
     except KeyError:
         print(f"Caught KeyError: {key}\n")
-    
+
 
 if __name__ == "__main__":
     print("=== Garden Error Types Demo ===")
     garden_operations()
+    test_error_types()
     pass
