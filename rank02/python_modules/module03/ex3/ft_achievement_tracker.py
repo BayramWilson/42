7db78773d8cea505ab78929d5fa7248be1ccbc71
Rammo Dev <bayram.wilson04@gmail.com>
2026-02-05 21:58:53 +0100

    done with ex3

--- rank02/python_modules/module03/ex3/ft_achievement_tracker.py
+++ rank02/python_modules/module03/ex3/ft_achievement_tracker.py
@@ -1,5 +1,6 @@
 import inspect
 import re
+from collections import Counter
 
 
 def varname(p):
@@ -14,6 +15,19 @@ def print_player_achievements(playername: str, player: set) -> None:
     print(f"Player {playername} achievements: {player}")
 
 
+def unique(s: list) -> set:
+    temp = []
+    for i in s:
+        temp.extend(list(i))
+
+    c = Counter(temp)
+    result = set()
+    for k, v in c.items():
+        if v == 1:
+            result.add(k)
+    return result
+
+
 if __name__ == "__main__":
     print("=== Achievement Tracker System ===\n")
     alice = {'first_kill', 'level_10', 'treasure_hunter', 'speed_demon'}
@@ -21,11 +35,15 @@ if __name__ == "__main__":
     charlie = {'level_10', 'treasure_hunter', 'boss_slayer', 'speed_demon',
                'perfectionist'}
     player = ['Alice', 'Bob', 'Charlie']
+    all_sets = [alice, bob, charlie]
+
     unique_achievements = set.union(alice, bob, charlie)
     common = set.intersection(alice, bob, charlie)
-    rare = set.difference(alice, bob)
-    more_rare = set.difference(bob, charlie)
-    more_more_rare = set.symmetric_difference(alice, charlie, bob)
+    alice_bob_common = set.intersection(alice, bob)
+    alice_vs_bob_unique = set.difference(alice, bob)
+    bob_vs_alice_uique = set.difference(bob, alice)
+    rare = unique(all_sets)
+
     print_player_achievements(player[0], alice)
     print_player_achievements(player[1], bob)
     print_player_achievements(player[2], charlie)
@@ -34,8 +52,7 @@ if __name__ == "__main__":
     print(f"All unique achievements {unique_achievements}")
     print(f"Total unique achievements: {len(unique_achievements)}\n")
     print(f"Common to all players: {common}")
-    print(rare)
-    print(more_rare)
-    print(more_more_rare)
-    # print(f"Rare achievements (1 Player): {more_rare}")
-    first_kill = False
+    print(f"Rare achievements (1 player): {rare}\n")
+    print(f"Alice vs Bob common: {alice_bob_common}")
+    print(f"alice unique: {alice_vs_bob_unique}")
+    print(f"bob unique: {bob_vs_alice_uique}")
